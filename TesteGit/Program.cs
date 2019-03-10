75b7cd6b3dc490992f1b22936fc15ecf96a790ca
URM <emailteste@email.com>
2019-03-10 16:02:25 -0300

    Acrescentado o desejando bom dia

--- TesteGit/Program.cs
+++ TesteGit/Program.cs
@@ -7,6 +7,7 @@ namespace TesteGit
         static void Main(string[] args)
         {
             Console.WriteLine("Teste github!");
+            Console.WriteLine("Tenha um bom dia!");
         }
     }
 }
